f557eb3e2d871ab619469bb351266a93cb8cbca5
Drew Roberts <github@drewroberts.com>
2021-02-14 07:31:12 -0500

    Remove models & policies since not created yet

--- src/RemoteMonitoringServiceProvider.php
+++ src/RemoteMonitoringServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\RemoteMonitoring;
 
-use Tipoff\RemoteMonitoring\Models\RemoteMonitoring;
-use Tipoff\RemoteMonitoring\Policies\RemoteMonitoringPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -14,9 +12,6 @@ class RemoteMonitoringServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
-            ->hasPolicies([
-                RemoteMonitoring::class => RemoteMonitoringPolicy::class,
-            ])
             ->name('remote-monitoring')
             ->hasConfigFile();
     }
