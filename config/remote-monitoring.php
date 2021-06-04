663baee45ee6bee290de504d1284560a5ede863e
Kyle Barney <kyle@kylebarney.dev>
2021-06-03 23:25:29 -0600

    Add routing configuration variables

--- config/remote-monitoring.php
+++ config/remote-monitoring.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-
+    'routes' => [
+        'prefix' => env('REMOTE_MONITORING_ROUTE_PREFIX', 'remote-monitoring'),
+        'middleware' => [
+            'auth',
+        ],
+    ],
 ];
